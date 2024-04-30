db0aeec57dc38313387c9f7585edeabdc519672f
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-04-30 14:08:59 +0800

    Create StockMarket.java

---
+++ Lab Exercises 8/src/StockMarket.java
@@ -0,0 +1,21 @@
+public class StockMarket {
+    private final String symbol, name;
+    double previousClosingPrice, currentPrice;
+
+    public StockMarket(String symbol, String name) {
+        this.symbol = symbol;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        StockMarket sp500 = new StockMarket("SP500", "S&P 500");
+        sp500.previousClosingPrice = 144.59;
+        sp500.currentPrice = 144.75;
+
+        System.out.println(sp500.name + " | " + sp500.symbol + " | " + sp500.getChangePercent());
+    }
+
+    public double getChangePercent() {
+        return (currentPrice - previousClosingPrice) / previousClosingPrice;
+    }
+}
