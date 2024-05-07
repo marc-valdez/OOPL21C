056aae3f798a275cefbad28f32b629f8ca4149c7
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-05-07 15:36:43 +0800

    Update Main.java

--- Midterm Project/src/Main.java
+++ Midterm Project/src/Main.java
@@ -6,14 +6,15 @@ import java.util.Scanner;
 public class Main {
     static final Byte MAX = 6;
     static final String REGEX = "^[1-6] [1-6] [1-6] [1-6]$";
-    static Byte GUESSES = 12;
+    static Byte MAXGUESSES = 12;
+    static Byte guessCount = MAXGUESSES;
     
     /**
      * Generates a new code consisting of 4 random numbers between 1 and MAX.
      *
      * @return a list of 4 random numbers between 1 and MAX
      */
-    public static List<Byte> newCode() {
+    public static List<Byte> newRandomCode() {
         List<Byte> numberBank = new ArrayList<>();
         for (byte i = 1; i <= MAX; i++) {
             numberBank.add(i);
@@ -30,12 +31,62 @@ public class Main {
      * @param memory   the list of bytes representing the memory
      * @return the generated code as a list of bytes
      */
-    public static List<Byte> newCode(String feedback, List<Byte> memory) {
+    public static List<Byte> newRandomCode(String feedback, List<Byte> memory) {
         if (feedback.length() == 4) {
             Collections.shuffle(memory);
             return memory;
         }
-        return newCode();
+        return newRandomCode();
+    }
+
+    public static Byte randomNumber() {
+        byte buffer;
+        do  {
+            buffer = (byte)(((Math.random() * 6)) + 1);
+        } while (inUse.contains(buffer));
+        return buffer;
+    }
+
+    /*
+    generate a number
+    check if number is in inUse
+    if in use generate new number
+    if not, then return
+     */
+
+    static List<List<Byte>> guessBank = new ArrayList<>();
+    static String[] testBank = new String[MAXGUESSES];
+    static List<Byte> inUse = new ArrayList<>();
+    static List<Byte> finalNumbers = new ArrayList<>();
+
+    public static List<Byte> newGuess() {
+        int index = MAXGUESSES - guessCount;
+
+        inUse = finalNumbers;
+
+        List<Byte> guess;
+        if (index == 0) {
+            guess = newRandomCode();
+        } else {
+            guess = guessBank.get(index - 1);
+            for (int i = 0; i < testBank[index-1].length(); i++) {
+
+                if(testBank[index-1].charAt(i) == '*') {
+                    finalNumbers.add(guessBank.get(index - 1).get(i));
+                } else {
+                    Byte newNumber = randomNumber();
+                    guess.set(i, newNumber);
+                    inUse.add(newNumber);
+
+                }
+
+            }
+        }
+
+        inUse.clear();
+
+        guessBank.add(guess);
+        return guess;
     }
 
     /**
@@ -59,7 +110,7 @@ public class Main {
      * @return the list of bytes parsed from the user input
      */
     @Deprecated
-    public static List<Byte> newGuess(String prompt) {
+    public static List<Byte> newUserGuess(String prompt) {
         while (true) {
             System.out.print(prompt);
             String input = new Scanner(System.in).nextLine();
@@ -99,6 +150,9 @@ public class Main {
                 feedback.append("#"); // Append '#' to indicate a misplaced guess
                 System.out.print("# ");
             }
+            else {
+                feedback.append("-"); // Append '#' to indicate a misplaced guess
+            }
         }
 
         return feedback.toString();
@@ -112,16 +166,18 @@ public class Main {
      * @param args the command-line arguments
      */
     public static void main(String[] args) {
-        List<Byte> secret = newCode();
+        List<Byte> secret = newRandomCode();
 
         String feedback = "";
-        List<Byte> memory = newCode();
-        while (GUESSES > 0) {
-            List<Byte> guess = newCode(feedback, memory);
+        List<Byte> memory = newRandomCode();
+        while (guessCount > 0) {
+            List<Byte> guess = newGuess();
 
             System.out.println("Guess: " + guess);
 
             feedback = testGuess(guess, secret);
+            testBank[MAXGUESSES - guessCount] = feedback;
+
             System.out.println(); // Line break for readability
 
             if (feedback.equals("****")) {
@@ -130,10 +186,10 @@ public class Main {
             }
             memory = guess;
 
-            System.out.println("You have " + GUESSES + " remaining guesses.");
-            GUESSES--;
+            System.out.println("You have " + guessCount + " remaining guesses.");
+            guessCount--;
 
-            if (GUESSES == 0) {
+            if (guessCount == 0) {
                 System.out.println("You ran out of guesses. The code was: " + secret);
             }
         }
